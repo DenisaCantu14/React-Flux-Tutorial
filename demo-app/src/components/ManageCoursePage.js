2af9d0bcb48f56c25d5ee4bddef96538bd782b25
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-24 18:17:22 +0300

    forms

--- demo-app/src/components/ManageCoursePage.js
+++ demo-app/src/components/ManageCoursePage.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import {Prompt} from "react-router-dom"
-const ManageCoursePage = props => {
-    return (
-        <>
-        <h2>Manage Course</h2>
-        <Prompt when={true} message="Are you sure you want to leave?"></Prompt>
-        {props.match.params.slug}
-        </>
-    )
-}
-export default ManageCoursePage
+import React, { useState, useEffect } from "react";
+import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
+
+const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: "",
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  function handleChange({ target }) {
+    setCourse({ ...course, [target.name]: target.value });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author ID is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved!");
+    });
+  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+      <Prompt when={true} message="Are you sure you want to leave?"></Prompt>
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </>
+  );
+};
+export default ManageCoursePage;
