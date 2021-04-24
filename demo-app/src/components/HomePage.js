9862b38320198c399933bb67fd05b53e2ec72f55
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-24 16:37:59 +0300

    React Router

--- demo-app/src/components/HomePage.js
+++ demo-app/src/components/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {Link}  from 'react-router-dom';
 
 function HomePage(){
 return(
     <div className="jumbotron">
         <h1>Home</h1>
         <p>React flux</p>
+        <Link to ={"/about"} className="btn btn-primary">About</Link>
     </div>
 );
 }
--- demo-app/src/components/common/Header.js
+++ demo-app/src/components/common/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
-function Header (){
-    return(
-        <nav>
-            <a href = "/">Home</a> | <a href = "/about">About</a> | <a href = "/courses">Courses</a>
-        </nav>
-    )
+function Header() {
+  const activeStyle = { color: "orange" };
+  return (
+    <nav>
+      <NavLink activeStyle={activeStyle} exact to="/">
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={activeStyle} to="/about">
+        About
+      </NavLink>
+      {" | "}
+      <NavLink activeStyle={activeStyle} to="/courses">
+        Courses
+      </NavLink>
+    </nav>
+  );
 }
 export default Header;
