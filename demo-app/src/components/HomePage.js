73de314db012c47600026e406d950ea4bb00d12b
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-24 14:04:50 +0300

    creating react components

---
+++ demo-app/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function HomePage(){
+return(
+    <div className="jumbotron">
+        <h1>Home</h1>
+        <p>React flux</p>
+    </div>
+);
+}
+
+export default HomePage;
