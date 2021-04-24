4335d1f3acb04004e915a66199448433fa6e009a
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-24 15:42:30 +0300

    updated components

--- demo-app/src/components/App.js
+++ demo-app/src/components/App.js
@@ -2,21 +2,20 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-
+import CoursesPage from "./CoursesPage";
 
 function App() {
-    function getPage(){
-        const route = window.location.pathname;
-    if (route === "/about")
-        return <AboutPage />;
-    return <HomePage/>; 
-    }
-    return(
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
     <div className="container-fluid">
-         <Header />
-         {getPage()}
-    </div>)
-   
-     
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 export default App;
---
+++ demo-app/src/components/CoursesPage.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList"
+function CoursesPage () {
+  const [courses, setCourse] = useState([])
+  useEffect(() => {
+      getCourses().then( _courses => setCourse(_courses));
+  }, [])
+    return (
+      <>
+        <h2>Courses</h2>
+        <CourseList courses={courses} />
+      </>
+    );
+  }
+
+export default CoursesPage;
